0f09fd3b9607ef255495a59359b2c3965a65e897
KimuraTomoki <krmmmmmmrk@gmail.com>
2017-05-23 21:02:03 +0900

    一旦完成

---
+++ js/master.js
@@ -0,0 +1,29 @@
+var winScroll = 0;
+
+$(window).scroll(function(){
+    winScroll = $(window).scrollTop();
+    if(winScroll > 320) {
+        $('.Page_Contents_Image1').css({
+            'opacity':1,
+            'transition':'1s'
+        });
+    }
+    if(winScroll > 820) {
+        $('.Page_Contents_Image2').css({
+            'opacity':1,
+            'transition':'1s'
+        });
+    }
+    if(winScroll > 1320) {
+        $('.Page_Contents_Image3').css({
+            'opacity':1,
+            'transition':'1s'
+        });
+    }
+    if(winScroll > 1820) {
+        $('.Page_Contents_Image4').css({
+            'opacity':1,
+            'transition':'1s'
+        });
+    }
+});
